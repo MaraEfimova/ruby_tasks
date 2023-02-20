56ac001a05a61a43c5f6fe45395ccaf303a742be
Maria Efimova <marivaefi@gmail.com>
2023-02-20 18:26:51 +0200

    The script returns either an error to the user or true.

--- task01.rb
+++ task01.rb
@@ -1,10 +1,12 @@
 #1. Write a script that returns true if a string contains comparison, e.g. "<23", ">45".
 
 def task01(str)
-  str.gsub!(/>>/, "")
-  str.gsub!(/<</, "")
   str.gsub!(/ /, "")
-  str.gsub!(/([<,>,=])(\d+)(\+)/) {">=" + $2}
-  str.gsub!(/([<,>,=])(\d+)(\-)/) {"<=" + $2}
-  str.include?(">") || str.include?("<")
+  if str.scan(/[<>]=?\d+/)[0].length == str.length 
+    true
+  else
+    "Error! The input must be comparison symbol with integer number after that. Spaces are optional."
+  end
 end
+
+p task01(">=8764583674583567")
