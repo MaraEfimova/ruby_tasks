5a52d5f1faed480eb3d8476b35ea5e5d215f6b6e
Maria Efimova <marivaefi@gmail.com>
2023-02-20 17:07:06 +0200

    The first attempt

--- task01.rb
+++ task01.rb
@@ -1,4 +1,10 @@
-##body of task
+#1. Write a script that returns true if a string contains comparison, e.g. "<23", ">45".
+
 def task01(str)
-  str
+  str.gsub!(/>>/, "")
+  str.gsub!(/<</, "")
+  str.gsub!(/ /, "")
+  str.gsub!(/([<,>,=])(\d+)(\+)/) {">=" + $2}
+  str.gsub!(/([<,>,=])(\d+)(\-)/) {"<=" + $2}
+  str.include?(">") || str.include?("<")
 end
