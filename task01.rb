5ba4c1a3229fecab21fd813478e306e816500458
Maria Efimova <marivaefi@gmail.com>
2023-02-21 13:05:20 +0200

    Change the error appearance in task01

--- task01.rb
+++ task01.rb
@@ -2,11 +2,11 @@
 
 def task01(str)
   str.gsub!(/ /, "")
-  if str.scan(/[<>]=?\d+/)[0].length == str.length 
+  if str.scan(/[<>]?=?\d+/)[0].length == str.length 
     true
+  elsif str.scan(/\d+/)[0].length == str.length 
+    false
   else
-    "Error! The input must be comparison symbol with integer number after that. Spaces are optional."
+    raise "Error! The input must be comparison symbol with integer number after that. Spaces are optional."
   end
 end
-
-p task01(">=8764583674583567")
---
+++ task02.rb
@@ -0,0 +1,5 @@
+#2. For a given array that contains some strings output the content of those strings separated with whitespace between them, having capitalized first word and ending with ".", e.g. [[" thIs ", "Is"], nil, " My  ", ["   first"], "", :program] should output "This is my first program."
+
+def task02(arr)
+    
+end
