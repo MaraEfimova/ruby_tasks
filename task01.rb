1aba47b6c56b686547e576143a515d403c60425b
Maria Efimova <marivaefi@gmail.com>
2023-02-23 12:15:25 +0200

    Some minor fixes

--- task01.rb
+++ task01.rb
@@ -1,12 +1,15 @@
 #1. Write a script that returns true if a string contains comparison, e.g. "<23", ">45".
 
 def task01(str)
+  if !str.scan(/[<>]? =/).empty? || !str.scan(/ [<>]/).empty?
+    raise "No spaces before '>, <, =' symols"
+  end
   str.gsub!(/ /, "")
   if str.scan(/[<>]?=?\d+/)[0].length == str.length 
     true
   elsif str.scan(/\d+/)[0].length == str.length 
     false
   else
-    raise "Error! The input must be comparison symbol with integer number after that. Spaces are optional."
+    raise "The input must be comparison symbol with integer number after that. Spaces are optional."
   end
 end
