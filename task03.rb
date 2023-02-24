0c3b16ef8a04f90ff7e46c583f685e734df9c3ab
Maria Efimova <marivaefi@gmail.com>
2023-02-24 12:14:32 +0200

    First draft of task03 structure

--- task03.rb
+++ task03.rb
@@ -1,1 +1,9 @@
 #[#3] Task03 - For a given array that contains the string "foo" - output it at the end without changing the places of the other items in the array, e.g. [1, :here, nil, "some", "foo", "thing", ""] should output [1, :here, nil, "some", "thing", "", "foo"] 
+
+def task03(arr)
+    raise "Array is empty. This is not valid" if arr.empty?
+    arr1, arr2 = arr.partition{|elem| elem != "foo"}
+    arr1 + arr2
+end
+
+p task03([1, :here, nil, "some", "foo", "foo", "thing", ""])
