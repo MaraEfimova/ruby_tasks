9a83e4e4cfa4e27534cabebc986165562599da56
Maria Efimova <marivaefi@gmail.com>
2023-02-24 12:34:04 +0200

    Creating of task04 file

---
+++ task04.rb
@@ -0,0 +1,5 @@
+#For a given array of elements show the type of each element but keep the array structure, e.g. [["hi"], {}, :sym, [1, true], nil] should output [[String], Hash, Symbol, [Integer, TrueClass], NilClass]
+
+def task04(arr)
+    arr
+end
