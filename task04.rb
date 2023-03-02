8265af322bba7eab65fc4ef3d83b66a6b61fd645
Maria Efimova <marivaefi@gmail.com>
2023-03-02 10:22:53 +0200

    Write first variant of task04 code

--- task04.rb
+++ task04.rb
@@ -1,5 +1,38 @@
 #For a given array of elements show the type of each element but keep the array structure, e.g. [["hi"], {}, :sym, [1, true], nil] should output [[String], Hash, Symbol, [Integer, TrueClass], NilClass]
 
 def task04(arr)
-    arr
+	result_array = []
+
+	def check_type(el, result_array)
+		if el.is_a?(Array)
+			if el.empty?
+				Array
+			else
+				temp_arr = []
+				el.each do |arr_elem|
+					temp_arr << check_type(arr_elem, result_array)
+				end
+				temp_arr
+			end
+		elsif el.class == Hash
+			if el.empty?
+				Hash
+			else
+				p "it is not an empty Hash"
+				temp_hash = {}
+				el.each do |hash_key, hash_value|
+					temp_hash.store(check_type(hash_key, result_array), check_type(hash_value, result_array))
+				end
+				temp_hash
+			end
+		else
+			el.class
+		end
+	end
+
+	arr.each do |el|
+		result_array << (check_type(el, result_array))
+	end
+
+	result_array
 end
