0efc43149f0fca9453553005f1a87c50f92640ed
Maria Efimova <marivaefi@gmail.com>
2023-03-02 12:14:27 +0200

    Added check for NIL

--- task03.rb
+++ task03.rb
@@ -1,7 +1,9 @@
 #[#3] Task03 - For a given array that contains the string "foo" - output it at the end without changing the places of the other items in the array, e.g. [1, :here, nil, "some", "foo", "thing", ""] should output [1, :here, nil, "some", "thing", "", "foo"] 
 
 def task03(arr)
-    raise "Array is empty. This is not valid" if arr.empty?
+    if arr.nil? || arr.empty?
+        raise "Array is empty. This is not valid"
+    end
     arr1, arr2 = arr.partition{|elem| elem.class == String && elem.strip.downcase == "foo"}
     arr2 + arr1
 end
