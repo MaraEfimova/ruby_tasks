58762331b377e34acc7d3a3100af8c06fdcb7a5b
Maria Efimova <marivaefi@gmail.com>
2023-03-09 11:53:19 +0200

    Added checking type

--- task04.rb
+++ task04.rb
@@ -1,6 +1,13 @@
 #For a given array of elements show the type of each element but keep the array structure, e.g. [["hi"], {}, :sym, [1, true], nil] should output [[String], Hash, Symbol, [Integer, TrueClass], NilClass]
 
 def task04(arr)
+	if arr.class != Array
+		raise "Not a valid format. Need an array"
+	end
+	if arr.nil? || arr.empty?
+		raise "Array is empty. This is not valid"
+	end
+
 	result_array = []
 
 	def check_type(el, result_array)
@@ -18,7 +25,6 @@ def task04(arr)
 			if el.empty?
 				Hash
 			else
-				p "it is not an empty Hash"
 				temp_hash = {}
 				el.each do |hash_key, hash_value|
 					temp_hash.store(check_type(hash_key, result_array), check_type(hash_value, result_array))
