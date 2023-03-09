8db33b08bbd47be8144b9fb42d2ca2da88aa6d6c
Maria Efimova <marivaefi@gmail.com>
2023-03-09 11:19:02 +0200

    Add some validation, fixed bags according to revievers comments

--- task02.rb
+++ task02.rb
@@ -1,20 +1,28 @@
 #2. For a given array that contains some strings output the content of those strings separated with whitespace between them, having capitalized first word and ending with ".", e.g. [[" thIs ", "Is"], nil, " My  ", ["   first"], "", :program] should output "This is my first program."
 
 def task02(arr)
-  format_array = arr.flatten.join()
-  p format_array
-  if format_array.empty? || !format_array.scan(/[^\s\w,-]/).empty?
-    raise "Not a valid input. Letters, numbers, commas and dash are allowed in logical order."
-  elsif !format_array.scan(/\A[,]/).empty?
-    raise "Not a valid input. A sentence cannot start with a comma. Only letters, numbers and dash are allowed."
-  elsif !format_array.scan(/[,-]\z/).empty?
-    raise "Not a valid input. A sentence cannot end with a comma or dash. Only letters and numbers are allowed."
-  else
-    format_array = format_array.squeeze(" ")
-    if !format_array.scan(/[a-zA-Z]/).empty?
-      format_array = format_array.downcase.strip
-      format_array.sub!(/[a-zA-Z]/) {|string| string[0].upcase}
-    end
-    format_array += "."
+  if arr.class != Array
+    raise "The input type is invalid, expect an array"
   end
+  if arr.empty?
+    raise "The input is empty."
+  end
+
+  format_array = arr.flatten.join(" ").strip!
+  if format_array.empty?
+    raise "Not a valid input. The input had only empty data."
+  end
+  if !format_array.scan(/[^\s\w,-]/).empty? || !format_array.scan(/_/).empty?
+    raise "Not a valid input. Letters, numbers, commas and dash are allowed in logical order."
+  end
+  if !format_array.scan(/\A[,]/).empty?
+    raise "Not a valid input. A sentence cannot start with a comma. Only letters, numbers and dash are allowed."
+  end
+  if !format_array.scan(/[,-]\z/).empty?
+    raise "Not a valid input. A sentence cannot end with a comma or dash. Only letters and numbers are allowed."
+  end
+
+  format_array = format_array.squeeze(" ").downcase.strip
+  format_array.sub!(/[a-zA-Z]/) {|string| string[0].upcase}
+  format_array += "."
 end
