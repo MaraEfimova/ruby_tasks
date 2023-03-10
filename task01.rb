5335003fc82cceb10d0de649ad92662718e3bdc2
Maria Efimova <marivaefi@gmail.com>
2023-03-10 12:57:52 +0200

    Change logic of checking comperison in string, added only one regular expression.

--- task01.rb
+++ task01.rb
@@ -1,15 +1,12 @@
 #1. Write a script that returns true if a string contains comparison, e.g. "<23", ">45".
 
 def task01(str)
-  if !str.scan(/[<>]? =/).empty? || !str.scan(/ [<>]/).empty?
-    raise "No spaces before '>, <, =' symols"
-  end
-  str.gsub!(/ /, "")
-  if str.scan(/\d+/)[0].length == str.length  
+	if str.class != String
+		raise "Not a valid format. Need a string"
+	end
+  if str.scan(/\A\s*(<|>|<=|>=|=){1}\s*[1-9]+[0-9]*\s*\z/).empty?
     false
-  elsif str.scan(/[<>]?=?\d+/)[0].length == str.length
-    true
   else
-    raise "The input must be comparison symbol with integer number after that. Spaces are optional."
+    true
   end
 end
