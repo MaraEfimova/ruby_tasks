fe626dae1c375dcea78b0dcb18fcd26f194b46d6
Maria Efimova <marivaefi@gmail.com>
2023-03-14 16:22:48 +0200

    Specified errors and extend condition that checking _ sign

--- task02.rb
+++ task02.rb
@@ -2,27 +2,27 @@
 
 def task02(arr)
   if arr.class != Array
-    raise "The input type is invalid, expect an array"
+    raise ArgumentError, "The input type is invalid, expect an array"
   end
   if arr.empty?
-    raise "The input is empty."
+    raise ArgumentError, "The input is empty."
   end
 
-  format_array = arr.flatten.join(" ").strip!
-  if format_array.nil?
-    raise "Not a valid input. The input had only empty data."
+  format_string = arr.flatten.join(" ").strip!
+  if format_string.nil?
+    raise ArgumentError, "Not a valid input. The input had only empty data."
   end
-  if !format_array.scan(/[^\s\w,-]/).empty? || !format_array.scan(/_/).empty?
-    raise "Not a valid input. Letters, numbers, commas and dash are allowed in logical order."
+  if !format_string.scan(/([^\s\w,-]|_)/).empty?
+    raise ArgumentError, "Not a valid input. Letters, numbers, commas and dash are allowed in logical order."
   end
-  if !format_array.scan(/\A[,]/).empty?
-    raise "Not a valid input. A sentence cannot start with a comma. Only letters, numbers and dash are allowed."
+  if !format_string.scan(/\A[,]/).empty?
+    raise ArgumentError, "Not a valid input. A sentence cannot start with a comma. Only letters, numbers and dash are allowed."
   end
-  if !format_array.scan(/[,-]\z/).empty?
-    raise "Not a valid input. A sentence cannot end with a comma or dash. Only letters and numbers are allowed."
+  if !format_string.scan(/[,-]\z/).empty?
+    raise ArgumentError, "Not a valid input. A sentence cannot end with a comma or dash. Only letters and numbers are allowed."
   end
 
-  format_array = format_array.squeeze(" ").downcase.strip
-  format_array.sub!(/[a-zA-Z]/) {|string| string[0].upcase}
-  format_array += "."
+  format_string = format_string.squeeze(" ").downcase.strip
+  format_string.sub!(/[a-zA-Z]/) {|string| string[0].upcase}
+  format_string += "."
 end
