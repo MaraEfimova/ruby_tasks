2dbc2cd40e8a8e332c3b351dbb319dfa8d0fff53
Maria Efimova <marivaefi@gmail.com>
2023-03-15 12:19:54 +0200

    Specified error class

--- task01.rb
+++ task01.rb
@@ -2,7 +2,7 @@
 
 def task01(str)
 	if str.class != String
-		raise "Not a valid format. Need a string"
+		raise ArgumentError, "Not a valid format. Need a string"
 	end
   if str.scan(/\A\s*(<|>|<=|>=|=){1}\s*[1-9]+[0-9]*\s*\z/).empty?
     false
