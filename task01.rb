095d69b77e9298677b58046ac6b0bf68bed2acff
Maria Efimova <marivaefi@gmail.com>
2023-03-17 11:05:12 +0200

    Change logic of string shecking according to comments

--- task01.rb
+++ task01.rb
@@ -4,9 +4,11 @@ def task01(str)
 	if str.class != String
 		raise ArgumentError, "Not a valid format. Need a string"
 	end
-  if str.scan(/\A\s*(<|>|<=|>=|=){1}\s*[1-9]+[0-9]*\s*\z/).empty?
+  if !str.scan(/\A\s*[1-9]+[0-9]*\s*\z/).empty?
     false
-  else
+  elsif !str.scan(/\A\s*(<|>|<=|>=|=){1}\s*[1-9]+[0-9]*\s*\z/).empty?
     true
+  else
+    raise ArgumentError, "Not a valid format. Need a string with a comparison or number"
   end
 end
