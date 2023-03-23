39e011dd12b927a699405910b9ddeb40f6963cfe
Maria Efimova <marivaefi@gmail.com>
2023-03-23 16:24:58 +0200

    First draft of task06

--- task06.rb
+++ task06.rb
@@ -2,21 +2,23 @@
 # UPD: We allow dashes, brackets (no need to check the position and if it is a opening-closing pair of brackets), whitespace and "+", but only for the "+" the rule is that it should be the first symbol
 
 def task06(str)
-  p str
   if str.class != String
-		raise "Not a valid format. Need a string."
+		raise ArgumentError, "Not a valid format. Need a string."
 	end
 	if str.nil? || str.delete(" ").empty?
-		raise "String is empty."
+		raise ArgumentError, "String is empty."
+	end 
+
+	str.split("-").each do |s|
+		if s.scan(/\A[a-zA-Z]+\z|\A\s*[+]*[0-9]+\z/).empty?
+			raise ArgumentError, "Explain part of the number either with numbers or with a word."
+		end
 	end
   
-	p str
-	str1 = str.gsub(/[aAbBcC]/, "2").gsub(/[dDeEfF]/, "3").gsub(/[gGhHiI]/, "4").gsub(/[jJkKlL]/, "5").gsub(/[mMnNoO]/, "6").gsub(/[pPqQrRsS]/, "7").gsub(/[tTuUvV]/, "8").gsub(/[wWxXyYzZ]/, "9")
+	str.downcase().tr!('abcdefghijklmnopqrstuvwxyz', "22233344455566677778889999")
 
-	if !str1.scan(/\s*[+]?\s*[a-zA-Z0-9-]+\s*/).empty?
-		return str.gsub(/[aAbBcC]/, "2").gsub(/[dDeEfF]/, "3").gsub(/[gGhHiI]/, "4").gsub(/[jJkKlL]/, "5").gsub(/[mMnNoO]/, "6").gsub(/[pPqQrRsS]/, "7").gsub(/[tTuUvV]/, "8").gsub(/[wWxXyYzZ]/, "9")
+	if !str.scan(/\s*[+]?\s*[a-zA-Z0-9-]+\s*/).empty?
+		return str
 	end
-	raise "Not a valid string. Only plus sign, dashes, numbers and letters are allowed."
+	raise ArgumentError, "Not a valid string. Only plus sign, dashes, numbers and letters are allowed."
 end
-
-p task06("   +1-800-+HElLo")
