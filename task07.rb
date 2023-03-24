0df9ce918ec681a729e3c4ba3d4ad291b4885f0b
Maria Efimova <marivaefi@gmail.com>
2023-03-24 14:01:12 +0200

    Firs version of task07

--- task07.rb
+++ task07.rb
@@ -1,18 +1,24 @@
 # Output how many square roots are between numbers x and y, e.g. if x=8 and y=37 it should output "4" for 9, 16, 25 and 36
 
 def task07(arr)
-  if arr.class != Array
-		raise ArgumentError, "Not a valid format. Need an array"
-	end
-	if arr.length != 2
-		raise ArgumentError, "Array must contain 2 numbers"
-	end
+  raise ArgumentError, "Not a valid format. Need an array" if arr.class != Array
+	raise ArgumentError, "Array must contain 2 numbers" if arr.length != 2
   arr.each do |i|
-    if !(arr[0].class == Integer || arr[0].class == Float) && !(arr[1].class == Integer || arr[1].class == Float)
-      raise ArgumentError, "Array must contain 2 numbers"
-    end
+    raise ArgumentError, "Array must contain 2 numbers" if !(i.class == Integer || i.class == Float)
   end
-  (arr[0]...arr[1]).select()
-end
+  raise ArgumentError, "Second number must be positive or zero" if arr[1] < 0
+	raise ArgumentError, "First number can not be bigger than second." if arr[0] > arr[1]
 
-p task07([7.6, 37])
+	def is_square?(x)
+		(Math.sqrt(x) % 1).zero?
+	end
+	
+	arr[0] = 0 if arr[0] < 0
+	if arr[0] == arr[1] && is_square?(arr[0])
+		return 1
+	end
+
+  min = Math.sqrt(arr[0]).to_i + 1
+  max = Math.sqrt(arr[1]).to_i
+  (max - min) + 1
+end
