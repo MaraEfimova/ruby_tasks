9c42354156c3e8987d452bf268f275083e7f7f00
Maria Efimova <marivaefi@gmail.com>
2023-03-28 14:03:51 +0300

    Formatting tabs

--- task01.rb
+++ task01.rb
@@ -1,9 +1,9 @@
 #1. Write a script that returns true if a string contains comparison, e.g. "<23", ">45".
 
 def task01(str)
-	if str.class != String
-		raise ArgumentError, "Not a valid format. Need a string"
-	end
+  if str.class != String
+    raise ArgumentError, "Not a valid format. Need a string"
+  end
   if !str.scan(/\A\s*[1-9]+[0-9]*\s*\z/).empty?
     false
   elsif !str.scan(/\A\s*(<|>|<=|>=|=){1}\s*[1-9]+[0-9]*\s*\z/).empty?
