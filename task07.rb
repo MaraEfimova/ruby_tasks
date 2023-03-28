c5a6315b43108396ed4a3ee43e11907a1cbe6150
Maria Efimova <marivaefi@gmail.com>
2023-03-28 14:00:49 +0300

    Formatting tabs

--- task07.rb
+++ task07.rb
@@ -2,21 +2,21 @@
 
 def task07(arr)
   raise ArgumentError, "Not a valid format. Need an array" if arr.class != Array
-	raise ArgumentError, "Array must contain 2 numbers" if arr.length != 2
+  raise ArgumentError, "Array must contain 2 numbers" if arr.length != 2
   arr.each do |i|
     raise ArgumentError, "Array must contain 2 numbers" if !(i.class == Integer || i.class == Float)
   end
   raise ArgumentError, "Second number must be positive or zero" if arr[1] < 0
-	raise ArgumentError, "First number can not be bigger than second." if arr[0] > arr[1]
+  raise ArgumentError, "First number can not be bigger than second." if arr[0] > arr[1]
 
-	def is_square?(x)
-		(Math.sqrt(x) % 1).zero?
-	end
+  def is_square?(x)
+    (Math.sqrt(x) % 1).zero?
+  end
 	
-	arr[0] = 0 if arr[0] < 0
-	if arr[0] == arr[1] && is_square?(arr[0])
-		return 1
-	end
+  arr[0] = 0 if arr[0] < 0
+  if arr[0] == arr[1] && is_square?(arr[0])
+    return 1
+  end
 
   min = Math.sqrt(arr[0]).to_i + 1
   max = Math.sqrt(arr[1]).to_i
