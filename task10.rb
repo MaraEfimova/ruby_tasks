9265fde191546f3877d48ae247119abb8d708f12
Maria Efimova <marivaefi@gmail.com>
2023-03-30 11:30:24 +0300

    task10 first draft

--- task10.rb
+++ task10.rb
@@ -1,7 +1,32 @@
 # Return the maximum product of three numbers in an array, so [-8, -9, 1, 2, 7] outputs 504 (which is -8*-9*7)
 
 def task10(arr)
-  
-end
+  raise ArgumentError, "Not a valid format. Need an array" if arr.class != Array
+  raise ArgumentError, "Array is empty. This is not valid" if arr.empty?
+  raise ArgumentError, "Not a valid format. Need an array with more then two integer or float numbers inside" if arr.length < 3 
+  arr.each do |number|
+    raise ArgumentError, "Not a valid format. Need an array with more then two integer or float numbers inside" if number.class != Integer && number.class != Float
+  end
 
-p task10([-8, -9, 1, 2, 7])
+  arr.sort!
+  arr = [arr[0], arr[1], arr[-1], arr[-2], arr[-3]].uniq.sort
+
+  max_product = arr[0]*arr[1]*arr[2]
+  n = arr.length
+  i = 0
+
+  while i < n-2 do
+    j = i + 1
+    while j < n -1 do
+      k = j + 1
+      while k < n do
+        max_product = [max_product, arr[i]*arr[j]*arr[k]].max
+        k += 1
+      end
+      j += 1
+    end
+    i += 1
+  end
+  
+  max_product
+end
