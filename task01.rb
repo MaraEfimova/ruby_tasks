922f91367c73da2119f2a0a0861136ed6965a644
Maria Efimova <marivaefi@gmail.com>
2023-03-31 15:06:30 +0300

    Change falsy cheking for more appropriate truthy checking

--- task01.rb
+++ task01.rb
@@ -4,9 +4,9 @@ def task01(str)
   if str.class != String
     raise ArgumentError, "Not a valid format. Need a string"
   end
-  if !str.scan(/\A\s*[1-9]+[0-9]*\s*\z/).empty?
+  if str.match(/\A\s*[1-9]+[0-9]*\s*\z/)
     false
-  elsif !str.scan(/\A\s*(<|>|<=|>=|=){1}\s*[1-9]+[0-9]*\s*\z/).empty?
+  elsif str.match(/\A\s*(<|>|<=|>=|=){1}\s*[1-9]+[0-9]*\s*\z/)
     true
   else
     raise ArgumentError, "Not a valid format. Need a string with a comparison or number"
