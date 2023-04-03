19f9c58cd241a921eb216890dc817b999a5c22a4
Maria Efimova <42145670+MaraEfimova@users.noreply.github.com>
2023-04-03 16:49:05 +0300

    [#7] Task03 - Finding and adding "foo" at the end of the array (#6)
    * Deleted testing line at the end
    * Add some condition for variables
    * Added check for NIL
    * Added checking data type
    * Changed validation order and specified error types

---
+++ task03.rb
@@ -0,0 +1,13 @@
+#[#3] Task03 - For a given array that contains the string "foo" - output it at the end without changing the places of the other items in the array, e.g. [1, :here, nil, "some", "foo", "thing", ""] should output [1, :here, nil, "some", "thing", "", "foo"] 
+
+def task03(arr)
+  if arr.class != Array
+    raise ArgumentError, "Not a valid format. Need an array"
+  end
+  if arr.empty?
+    raise ArgumentError, "Array is empty. This is not valid"
+  end
+  
+  arr1, arr2 = arr.partition{|elem| elem.class == String && elem.strip.downcase == "foo"}
+  arr2 + arr1
+end
