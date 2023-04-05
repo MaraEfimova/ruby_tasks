70294851a0a6122fac4b0472a7be276d5dff490e
Maria Efimova <42145670+MaraEfimova@users.noreply.github.com>
2023-04-05 17:10:31 +0300

    [#19] Task09 - Moving letters to the next letter in the alphabet (#18)
    Allowed whitespaces and change .split method for .chars

---
+++ task09.rb
@@ -0,0 +1,20 @@
+# In a string move the letters to the next letter in the alphabet, e.g. "quiz" should become "rvja".
+
+def task09(str)
+  raise ArgumentError, "Not a valid format. Need a string" if str.class != String
+  raise ArgumentError, "String is empty. This is not valid" if str.delete!(" ").empty?
+  raise ArgumentError, "Only letters are allowed" if str.scan(/^[a-zA-Z]+$/).empty?
+
+  final_str = ""
+  arr = str.chars
+  arr.each do |letter|
+    if letter == "z"
+      final_str += "a"
+    elsif letter == "Z"
+      final_str += "A"
+    else
+      final_str += letter.next()
+    end
+  end
+  final_str
+end
