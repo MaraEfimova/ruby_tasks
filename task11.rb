6371c58776575a761f178cceb3cc183499f8efba
Maria Efimova <marivaefi@gmail.com>
2023-04-10 19:54:20 +0300

    Added checking non-unic words like 'Ant' and 'ant'

--- task11.rb
+++ task11.rb
@@ -3,17 +3,18 @@
 def task11(arr)
   raise ArgumentError, "Not a valid format. Need an array." if arr.class != Array
   raise ArgumentError, "String is empty. This is not valid" if arr.empty?
-  raise ArgumentError, "Erray must contain only string inside." if arr.one? {|elem| elem.class != String}
+  raise ArgumentError, "Array must contain only string inside." if arr.one? {|elem| elem.class != String}
   arr.each do |elem|
+    elem.downcase!
     elem.strip!
     raise ArgumentError, "The element must contain at least one letter." if arr.one? {|elem| elem.empty?}
     raise ArgumentError, "The element must contain only one word" if !elem.match(/\A[a-zA-Z]+\z/)
   end
+  raise ArgumentError, "Array must contain only unique values." if arr.length != arr.uniq.length
 
   i = 0
   while i < arr.count-1 do
-    if arr[i][-1].downcase != arr[i+1][0].downcase
-      return false
+    if arr[i][-1] != arr[i+1][0]
     end
     i+=1
   end
