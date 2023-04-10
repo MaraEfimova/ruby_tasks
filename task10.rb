e9b1418f661adde6ede3c7d2e8dc290ae882e034
Maria Efimova <marivaefi@gmail.com>
2023-04-10 19:35:39 +0300

    Added possibility for using non-unic numbers. Changed error messages. Instead of using my own method added .combination method

--- task10.rb
+++ task10.rb
@@ -3,30 +3,22 @@
 def task10(arr)
   raise ArgumentError, "Not a valid format. Need an array" if arr.class != Array
   raise ArgumentError, "Array is empty. This is not valid" if arr.empty?
-  raise ArgumentError, "Not a valid format. Need an array with more then two integer or float numbers inside" if arr.length < 3 
+  raise ArgumentError, "Not a valid format. Need an array with more then two numbers inside" if arr.length < 3 
   arr.each do |number|
-    raise ArgumentError, "Not a valid format. Need an array with more then two integer or float numbers inside" if number.class != Integer && number.class != Float
+    raise ArgumentError, "Not a valid format. Elements must be Integer or Float numbers." if number.class != Integer && number.class != Float
   end
 
-  arr.sort!
-  arr = [arr[0], arr[1], arr[-1], arr[-2], arr[-3]].uniq.sort
-
-  max_product = arr[0]*arr[1]*arr[2]
-  n = arr.length
-  i = 0
-
-  while i < n-2 do
-    j = i + 1
-    while j < n -1 do
-      k = j + 1
-      while k < n do
-        max_product = [max_product, arr[i]*arr[j]*arr[k]].max
-        k += 1
-      end
-      j += 1
-    end
-    i += 1
+  if arr.length >= 5
+    arr.sort!
+    arr = [arr[0], arr[1], arr[-1], arr[-2], arr[-3]]
   end
+
+  multipl_arr = []
+  arr = arr.combination(3).to_a
   
-  max_product
+  arr.each do |elem| 
+    multipl_arr << elem[0]*elem[1]*elem[2]
+  end
+
+  multipl_arr.max
 end
