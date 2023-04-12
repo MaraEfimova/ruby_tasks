a44a86abdab180aa0e7ded19c005101154350662
Maria Efimova <marivaefi@gmail.com>
2023-04-12 12:04:17 +0300

    Replace while loop to each_with_index

--- task11.rb
+++ task11.rb
@@ -3,23 +3,22 @@
 def task11(arr)
   raise ArgumentError, "Not a valid format. Need an array." if arr.class != Array
   raise ArgumentError, "String is empty. This is not valid" if arr.empty?
-  raise ArgumentError, "Array must contain only string inside." if arr.one? {|elem| elem.class != String}
+  raise ArgumentError, "Array must contain only string inside." if arr.any? {|elem| elem.class != String}
   arr.each do |elem|
     elem.downcase!
     elem.strip!
-    raise ArgumentError, "The element must contain at least one letter." if arr.one? {|elem| elem.empty?}
     raise ArgumentError, "The element must contain only one word" if !elem.match(/\A[a-zA-Z]+\z/)
   end
   raise ArgumentError, "Array can not contain oly one word. It is not a game thought." if arr.length == 1
   raise ArgumentError, "Array must contain only unique values." if arr.length != arr.uniq.length
 
-  i = 0
-  while i < arr.count-1 do
-    if arr[i][-1] != arr[i+1][0]
-      return false
+  arr.each_with_index do |element, i|
+    if i < arr.length - 1
+      if arr[i][-1] != arr[i+1][0]
+        return false
+      end
     end
-    i+=1
   end
-  
+
   true
 end
