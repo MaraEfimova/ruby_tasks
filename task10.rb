d7b452a7f2dc47b89cc9ef3d4dc003831c579070
Maria Efimova <marivaefi@gmail.com>
2023-04-20 13:42:30 +0300

    Minor fixes

--- task10.rb
+++ task10.rb
@@ -3,7 +3,7 @@
 def task10(arr)
   raise ArgumentError, "Not a valid format. Need an array" if arr.class != Array
   raise ArgumentError, "Array is empty. This is not valid" if arr.empty?
-  raise ArgumentError, "Not a valid format. Need an array with more then two numbers inside" if arr.length < 3 
+  raise ArgumentError, "Not a valid format. Need an array with more then two numbers inside" if arr.length < 3
   arr.each do |element|
     raise ArgumentError, "Not a valid format. Elements must be Integer or Float numbers." if element.class != Integer && element.class != Float
   end
@@ -11,7 +11,7 @@ def task10(arr)
   multipl_arr = []
   arr = arr.combination(3).to_a
   
-  arr.each do |elem| 
+  arr.each do |elem|
     multipl_arr << elem[0]*elem[1]*elem[2]
   end
 
